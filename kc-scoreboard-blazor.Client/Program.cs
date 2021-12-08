c130e171914bf7c486ff4c76583ec5316250154e
Matthew Minish <mhm76@uclive.ac.nz>
2021-12-08 21:30:01 +1300

    Remove non git files and create .gitignore

---
+++ kc-scoreboard-blazor.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using kc_scoreboard_blazor.Client;
+using kc_scoreboard_blazor.Client.Services.Contracts;
+using kc_scoreboard_blazor.Client.Services.Implementations;
+using kc_scoreboard_blazor.Client.States;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
