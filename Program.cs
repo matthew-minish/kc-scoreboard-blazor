b42604a5011885e32d59d15ff7f2529d39e03acc
Matthew Minish <mhm76@uclive.ac.nz>
2021-12-08 21:25:15 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using kc_scoreboard_blazor.Server.Data;
+using kc_scoreboard_blazor.Server.Models;
+using kc_scoreboard_blazor.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlite("Filename=data.db"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = false;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+    options.Lockout.MaxFailedAccessAttempts = 10;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = 401;
+        return Task.CompletedTask;
+    };
+});
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+    {
+        //Note: Microsoft recommends to NOT migrate your database at Startup. 
+        //You should consider your migration strategy according to the guidelines
+        serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.Migrate();
+
+        var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<IdentityRole<Guid>>>();
+        await roleManager.CreateAsync(new IdentityRole<Guid>("SupportStaff"));
+
+        var userManager = serviceScope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+        var guest = new ApplicationUser() { UserName = "Guest" };
+        var guestPassword = "ajAOSIJD4589AS(*jdoija";
+
+        var matthew = new ApplicationUser() { UserName = "Matthew" };
+        var matthewPassword = "MatthewPassword123!";
+
+        await userManager.CreateAsync(guest, guestPassword);
+        await userManager.CreateAsync(matthew, matthewPassword);
+        await userManager.AddToRoleAsync(matthew, "SupportStaff");
+    }
+
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
